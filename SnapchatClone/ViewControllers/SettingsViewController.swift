d40b91de6bf0e27e981a05337390442928681d28
nolanzhong <nolan.zhong@duke.edu>
2023-03-03 18:49:46 -0500

    working on upload

--- SnapchatClone/ViewControllers/SettingsViewController.swift
+++ SnapchatClone/ViewControllers/SettingsViewController.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import Firebase
 
 class SettingsViewController: UIViewController {
 
@@ -16,6 +17,14 @@ class SettingsViewController: UIViewController {
     }
     
     @IBAction func logoutClicked(_ sender: Any) {
+        do {
+            try Auth.auth().signOut()
+            self.performSegue(withIdentifier: "toSignInVC", sender: nil)
+            
+        } catch {
+            
+        }
+        
     }
     
     /*
